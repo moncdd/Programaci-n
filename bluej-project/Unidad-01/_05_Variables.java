80a90cfbf288fb963e03dd2adec9ee2240e4a8a1
Nacho <nacho@ies.local>
2026-09-24 12:41:07 +0200

    Auto-sync: Thu Sep 24 12:41:07     2026

---
+++ bluej-project/Unidad-01/_05_Variables.java
@@ -0,0 +1,18 @@
+
+public class _05_Variables
+{
+    public static void main(String[] args)
+    {
+        // Declaración e inicialización de variables
+        int edad = 22;
+        double sueldo = 1824.23;
+        String nombre = "Pepito", apellido1 = "Ap1", apellido2 = "Ap2";
+        boolean aprobado = true;
+        
+        // Mostramos la información en pantalla
+        System.out.println("Bienvenido "+nombre+" "+apellido1+" "+apellido2);
+        System.out.println("Tienes "+edad);
+        System.out.println("Tu sueldo es "+sueldo);
+        System.out.println("¿Has aprobado Programación? "+aprobado);
+    }
+}
